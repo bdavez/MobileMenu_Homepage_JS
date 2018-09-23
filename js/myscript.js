32297d40a794400db5a0c09b64ffae5fe7198315
bdavez <bdavez@users.noreply.github.com>
2018-09-23 16:52:11 -0600

    Initial commit

---
+++ js/myscript.js
@@ -0,0 +1,7 @@
+function openOverlay() {
+    document.getElementById("overnav").style.height = "100%";
+  }
+  
+  function closeOverlay() {
+    document.getElementById("overnav").style.height = "0%";
+  }
